6b94547ebd9499fe39b03ca222e49866d863d015
Wanglala <sue4100035045@gmail.com>
2020-05-20 22:02:36 +0800

    feat: mongoose and record-Schema

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 3000
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongode error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.get('/', (req, res) => {
   res.send('project start!!')
