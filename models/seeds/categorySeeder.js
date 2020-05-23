422dd8e831cf7b7e4262ad17198597f78ad76877
Wanglala <sue4100035045@gmail.com>
2020-05-23 20:35:37 +0800

    fix: modify seed

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -2,12 +2,20 @@ const Category = require('../category')
 const categoryList = require('../seeds/category.json').results
 const db = require('../../config/mongoose')
 
-db.once('open', () => {
-  for (let i = 0; i < categoryList.length; i++) {
-    Category.create({
-      category: categoryList[i].category,
-      category_en: categoryList[i].category_en
+db.once('open', async () => {
+  //助教提供的方法
+  await Promise.all(
+    categoryList.map((category) => {
+      console.log(category);
+      return Category.create(category)
     })
-  }
+  )
+
+  // for (let i = 0; i < categoryList.length; i++) {
+  //   Category.create({
+  //     category: categoryList[i].category,
+  //     category_en: categoryList[i].category_en
+  //   })
+  // }
   console.log('Category seeds are created!!')
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -2,15 +2,26 @@ const Record = require('../record')
 const recordList = require('../seeds/record.json').results
 const db = require('../../config/mongoose')
 
-db.once('open', () => {
-  for (let i = 0; i < recordList.length; i++) {
-    Record.create({
-      name: recordList[i].name,
-      category: recordList[i].category,
-      date: recordList[i].date,
-      amount: recordList[i].amount,
-      modalId: recordList[i].modalId
+db.once('open', async () => {
+
+  //助教提供的async/await精簡語法 
+  await Promise.all(
+    recordList.map((record) => {
+      console.log(record);
+      return Record.create(record)
     })
-  }
+  )
   console.log('record seeds are created!!')
+  db.close()      //有這個才可以進行category的seed
+
+  // for (let i = 0; i < recordList.length; i++) {
+  //   Record.create({
+  //     name: recordList[i].name,
+  //     category: recordList[i].category,
+  //     date: recordList[i].date,
+  //     amount: recordList[i].amount,
+  //     modalId: recordList[i].modalId
+  //   })
+  // }
+  // console.log('record seeds are created!!')
 })
