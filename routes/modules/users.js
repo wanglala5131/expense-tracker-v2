4a4752cb4ba7345dcb1488673f36cad435a4c99d
Wanglala <sue4100035045@gmail.com>
2020-06-26 22:53:06 +0800

    feat: register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -17,7 +19,24 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('exist')
+        res.render('register', { name, email, password, confirmPassword })
+      } else {
+        User.create({
+          name,
+          email,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+          confirmPassword
+        })
+        res.redirect('/')
+      }
+    })
+    .catch(err => console.log(err))
+
 })
 
 module.exports = router
